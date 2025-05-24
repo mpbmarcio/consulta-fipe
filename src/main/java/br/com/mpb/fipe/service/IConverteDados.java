593f59216585962624e7ea17cab2f4c3e1db8cbe
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2025-05-24 00:34:08 -0300

    Atualizar com novo método.

--- src/main/java/br/com/mpb/fipe/service/IConverteDados.java
+++ src/main/java/br/com/mpb/fipe/service/IConverteDados.java
@@ -1,5 +1,8 @@
 package br.com.mpb.fipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
+    <T> List<T> obterDadosArray(String json, Class<T> classe);
     <T> T obterDados(String json, Class<T> classe);
 }
